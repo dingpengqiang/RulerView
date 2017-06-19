c3b503787e90225456baa6c172bd1b5dd0a7d026
dingpengqiang <ding.peng.qiang@foxmail.com>
2017-06-19 20:05:35 +0800

    提交

--- app/src/main/java/com/ding/pengqiang/rulerview/MainActivity.java
+++ app/src/main/java/com/ding/pengqiang/rulerview/MainActivity.java
@@ -2,12 +2,32 @@ package com.ding.pengqiang.rulerview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.ding.pengqiang.rulerview.weight.OnRulerChangeListener;
+import com.ding.pengqiang.rulerview.weight.RulerView;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+
+public class MainActivity extends AppCompatActivity implements OnRulerChangeListener {
+
+        TextView textView;
+        RulerView rulerView;
+        private int currLocation = 10000;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            rulerView = (RulerView) findViewById(R.id.rulerView);
+            rulerView.setOnRulerChangeListener(this);
+            rulerView.setCurrLocation(currLocation);
+            textView = (TextView) findViewById(R.id.tvValue);
+            textView.setText(currLocation + "");
+        }
+        @Override
+        public void onChanged(int newValue) {
+            textView.setText(newValue+"");
+        }
+
+
 }
