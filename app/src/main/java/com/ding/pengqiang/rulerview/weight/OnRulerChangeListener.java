883b6fe2104a2f279699f81933bdd5d2290e5bea
dingpengqiang <ding.peng.qiang@foxmail.com>
2017-06-19 20:01:36 +0800

    提交

---
+++ app/src/main/java/com/ding/pengqiang/rulerview/weight/OnRulerChangeListener.java
@@ -0,0 +1,10 @@
+package com.ding.pengqiang.rulerview.weight;
+
+/**
+ * Created by dingpengqiang on 2017/6/19 0019.
+ */
+
+public interface OnRulerChangeListener {
+
+    void onChanged(int newValue);
+}
